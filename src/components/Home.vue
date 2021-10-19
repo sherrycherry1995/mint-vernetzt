dbd19315d874cc285eb7ccedf6a8c7538ca6a3e1
Lisa-Sophie Kramer <lisa11cheyenne@googlemail.com>
2021-10-19 19:33:14 +0200

    added initial component

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  data: function () {
+    return {
+      title: 'Mint vernetzt',
+    }
+  },
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/sherrycherry1995/mint-vernetzt/'
+    ? '/mint-vernetzt/'
     : '/'
 }
